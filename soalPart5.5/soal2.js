0234dfb4e9459cec49cf43a22611a460fc366749
Hafidz Athaya <mhafidza528@gmail.com>
2023-09-05 10:54:18 +0800

    soal2IndexAccessing

---
+++ soalPart5.5/soal2.js
@@ -0,0 +1,27 @@
+// 2. Index Accessing - 1 by 1
+// Problem
+// Pada tugas ini kamu diminta untuk "memecah" sebuah kalimat dan menampilkan setiap kata didalamnya.
+// Untuk soal nomor ini, gunakan akses satu per satu karakter dari string untuk mengambil setiap huruf dalam kata.
+// Terasa manual? Tidak apa-apa, kita coba ini dulu untuk saat ini.
+
+// Hints
+// Saat kamu mendapatkan tiap huruf, untuk membentuk setiap kata kamu tinggal menggunakan simbol + untuk membentuk kata
+//  tersebut!
+
+let word = 'wow JavaScript is so cool';
+let firstWord = word[0] + word[1] + word[2];
+let secondWord = word[4] + word[5] + word[6] + word[7] + word[8] + word[9] + word[10] + word[11] + word[12] + word[13]
+let thirdWord ;
+let fourthWord ;
+let fifthWord ;
+
+console.log('First Word: ' + firstWord);
+console.log('Second Word: ' + secondWord);
+console.log('Third Word: '  );
+console.log('Fourth Word: '  );
+console.log('Fifth Word: '  );
+
+
+// ---------------------------------------
+console.log(' ');
+// ---------------------------------------
