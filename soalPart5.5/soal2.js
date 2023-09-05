0ebd49f30cd2ad71d38572dcbf079db0debfe389
Hafidz Athaya <mhafidza528@gmail.com>
2023-09-05 10:55:34 +0800

    soal2IndexAccessing

--- soalPart5.5/soal2.js
+++ soalPart5.5/soal2.js
@@ -11,17 +11,19 @@
 let word = 'wow JavaScript is so cool';
 let firstWord = word[0] + word[1] + word[2];
 let secondWord = word[4] + word[5] + word[6] + word[7] + word[8] + word[9] + word[10] + word[11] + word[12] + word[13]
-let thirdWord ;
-let fourthWord ;
-let fifthWord ;
+let thirdWord = word[15] + word[16]
+let fourthWord = word[18] + word[19]
+let fifthWord = word[21] + word[22] + word[23] + word[24]
 
+console.log('================================================');
 console.log('First Word: ' + firstWord);
 console.log('Second Word: ' + secondWord);
-console.log('Third Word: '  );
-console.log('Fourth Word: '  );
-console.log('Fifth Word: '  );
-
+console.log('Third Word: ' + thirdWord);
+console.log('Fourth Word: ' + fourthWord);
+console.log('Fifth Word: ' + fifthWord);
 
 // ---------------------------------------
-console.log(' ');
+console.log('================================================');
+console.log(`Ini adalah kalimatnya: ${firstWord} ${secondWord} ${thirdWord} ${fourthWord} ${fifthWord}`);
+console.log('================================================');
 // ---------------------------------------
