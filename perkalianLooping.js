aaad2eb2ba7f9ce4fc334cd863f07ee826611ab3
Hafidz Athaya <mhafidza528@gmail.com>
2023-09-09 14:40:15 +0800

    perkalian sederhana

---
+++ perkalianLooping.js
@@ -0,0 +1,9 @@
+console.log('----------------------------')
+console.log ("Perkalian 0 sampai dengan 10")
+console.log('----------------------------')
+for (let i = 0; i <= 10; i++) {
+    console.log (`Perkalian ${i}`)
+    for (let j = 0; j <= 10; j++) {
+        console.log (`${i} x ${j} = ${i * j}`)
+    } console.log('----------------------------')
+}
