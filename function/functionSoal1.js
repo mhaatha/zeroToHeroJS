780cdc9ce1af48bae83299576c5289ec3662fdb9
Hafidz Athaya <mhafidza528@gmail.com>
2023-09-12 10:50:00 +0800

    function soal 1, masih ez

---
+++ function/functionSoal1.js
@@ -0,0 +1,10 @@
+// Tugas 1
+// Buatlah sebuah fungsi bernama shoutOut(), yang mengembalikan nilai berupa "Halo Function!", 
+// yang kemudian akan ditampilkan di console.
+
+//bikin fungsinya disini
+function shoutOut(){
+    let text = "Halo Function!"
+    return text
+}
+console.log(shoutOut());
