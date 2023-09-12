4bd629ac44d6cc085c723b4cca7f7cc827e16fe1
Hafidz Athaya <mhafidza528@gmail.com>
2023-09-12 10:55:03 +0800

    soal 2 function, masih ez juga

---
+++ function/functionSoal2.js
@@ -0,0 +1,12 @@
+// Tugas 2
+// Buatlah sebuah fungsi bernama calculateMultiply(), yang mengembalikan nilai berupa hasil kali dari 
+// dua parameter yang dikirim.
+
+//bikin fungsinya disini
+function calculateMultiply(num1, num2) {
+    let hasil = num1 * num2;
+    return hasil;
+}
+
+hasilPerkalian = calculateMultiply(6,5);
+console.log(hasilPerkalian);
