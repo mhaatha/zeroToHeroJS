5e9cc3f23d208fab8a2c291797a32dc84f693dd1
mhaatha <mhafidza528@gmail.com>
2023-09-19 10:21:58 +0800

    quiz 2 function

---
+++ quizAfter7.5/quiz2.js
@@ -0,0 +1,24 @@
+// Problem
+// Diberikan sebuah function balikKata(kata) yang menerima satu parameter berupa string.
+// Function akan me-return kata yang dibalik. Contoh, jika kata adalah "John Doe", function akan me-return "eoD nhoJ".
+
+function balikKata(kata) {
+  let dibalik = "";
+  for (i = kata.length - 1; i >= 0; i--) {
+    // "John Doe" = length (8) -> 0,1,2,3,4,5,6,7
+    dibalik += kata[i];
+  }
+  return dibalik;
+}
+
+// TEST CASES
+console.log(balikKata("Hello World and Coders")); // sredoC dna dlroW olleH
+console.log(balikKata("John Doe")); // eoD nhoJ
+console.log(balikKata("I am a bookworm")); // mrowkoob a ma I
+console.log(balikKata("Coding is my hobby")); // ybboh ym si gnidoC
+console.log(balikKata("Super")); // repuS
+console.log(balikKata("Anjay"));
+
+// ------------------------------------
+console.log("");
+// ------------------------------------
