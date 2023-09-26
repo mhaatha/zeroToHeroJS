413a84c6a6685c2b21107aaab285917f8d6fa786
mhaatha <mhafidza528@gmail.com>
2023-09-26 09:20:53 +0800

    soal 2 2 hari

---
+++ soalPart8.6/soal2.js
@@ -0,0 +1,18 @@
+function perkalianUnik(arr) {
+  let penampung = []
+  let satu = 1;
+  for (i = 0; i < arr.length; i++) {
+    satu = satu * arr[i]
+  }
+  for (j = 0; j < arr.length; j++) {
+    penampung[j] = satu / arr[j]
+  }
+  return penampung
+}
+
+// TEST CASES
+console.log(perkalianUnik([2, 4, 6])); // [24, 12, 8]
+console.log(perkalianUnik([1, 2, 3, 4, 5])); // [120, 60, 40, 30, 24]
+console.log(perkalianUnik([1, 4, 3, 2, 5])); // [120, 30, 40, 60, 24]
+console.log(perkalianUnik([1, 3, 3, 1])); // [9, 3, 3, 9]
+console.log(perkalianUnik([2, 1, 8, 10, 2])); // [160, 320, 40, 32, 160]
