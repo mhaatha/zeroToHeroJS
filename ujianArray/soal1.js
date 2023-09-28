591dba3a734933b1bae33c7a352e9edf0799e242
mhaatha <mhafidza528@gmail.com>
2023-09-28 11:53:40 +0800

    revisi

--- ujianArray/soal1.js
+++ ujianArray/soal1.js
@@ -6,6 +6,7 @@ Diberikan sebuah function targetTerdekat(arr) yang menerima satu parameter berup
 
 function targetTerdekat(arr) {
   let penampung = [];
+  let posisiO = -1;
   for (i = 0; i < arr.length; i++) {
     if ("x" == arr[0]) {
       return 1;
@@ -18,9 +19,13 @@ function targetTerdekat(arr) {
     if (!arr.includes("x")) {
       return 0;
     } else if (arr[j] == "o") {
-      return penampung - [j];
+      const jarak = Math.abs(j - penampung)
+      if (jarak < penampung){
+        penampung = jarak
+      }
     }
   }
+  return parseInt(penampung);
 }
 
 // TEST CASES
@@ -29,5 +34,5 @@ console.log(targetTerdekat(["o", " ", " ", " ", "x", "x", "x"])); // 4
 console.log(targetTerdekat(["x", " ", " ", " ", "x", "x", "o", " "])); // 1
 console.log(targetTerdekat([" ", " ", "o", " "])); // 0
 console.log(targetTerdekat([" ", "o", " ", "x", "x", " ", " ", "x"])); // 2
-console.log(targetTerdekat(["o", "o", " ", " ", "x"])); // 4
-console.log(targetTerdekat(["o", "o", " ", " ", " "]));
+console.log(targetTerdekat(["o", "o", " ", " ", "x"])); // 3
+console.log(targetTerdekat(["o", "o", " ", " ", " "])); // 0
