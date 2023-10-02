0eb1965a6617d8037691745ec75451311cb5f34a
mhaatha <mhafidza528@gmail.com>
2023-10-02 19:37:11 +0800

    soal 1 susahhhhhhh, ntar bakalan kupelajarin lagi

---
+++ soalPart9.4/soal1.js
@@ -0,0 +1,64 @@
+/*
+Diberikan sebuah function digitPerkalianMinimum(angka) yang menerima satu parameter angka. Function akan mengembalikan jumlah digit minimal dari angka yang merupakan faktor angka parameter tersebut, Contoh, jika angka adalah 24, maka faktornya adalah 1 * 24, 2 * 12, 3 * 8, dan 4 * 6. Dari antara faktor tersebut, yang digit nya paling sedikit adalah 3 * 8 atau 4 * 6, sehingga function akan me-return 2.
+
+1 * 24 = 3 digit (124)
+2 * 12 = 3 digit (212)
+3 * 8 = 2 digit (38)
+4 * 6 = 2 digit (46)
+
+
+
+karena dibawah ini 2 digit dan terkecil
+3 * 8 = 2 digit (38)
+4 * 6 = 2 digit (46)
+
+ya jadikan dia minimum
+
+misal faktornya angka 1 hanyalah
+1 * 1 = 2 digit (11)
+
+Return 2
+
+Pisahkan dulu faktornya
+*/
+
+function digitPerkalianMinimum(angka) {
+  for (i = 1; i <= 10; i++) {
+    if (i === angka) {
+      return 2
+    }
+  }
+
+  let gabung = [];
+  let jumlah = [];
+  for (let i = 1; i <= angka; i++) {
+    if (angka % i === 0) {
+      jumlah.push(i);
+    }
+  }
+  for (let i = 0; i < jumlah.length; i++) {
+    for (let j = i + 1; j < jumlah.length; j++) {
+      if (jumlah[i] * jumlah[j] === angka) {
+        gabung.push(`${jumlah[i]}${jumlah[j]}`);
+      }
+    }
+  }
+
+  let panjangTerkecil = gabung[0].length;
+
+  for (let i = 1; i < gabung.length; i++) {
+    const panjangSaatIni = gabung[i].length;
+
+    if (panjangSaatIni < panjangTerkecil) {
+      panjangTerkecil = panjangSaatIni;
+    }
+  }
+  return panjangTerkecil
+}
+
+// TEST CASES
+console.log(digitPerkalianMinimum(24)); // 2
+console.log(digitPerkalianMinimum(90)); // 3
+console.log(digitPerkalianMinimum(20)); // 2
+console.log(digitPerkalianMinimum(179)); // 4
+console.log(digitPerkalianMinimum(1)); // 2
