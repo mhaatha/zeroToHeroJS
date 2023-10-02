2a28fbac37c84a4dfc8f6df320d9106a106f0f92
mhaatha <mhafidza528@gmail.com>
2023-10-03 07:10:38 +0800

    revisi penambahan algoritma, ini soal susah cuy asli

--- soalPart9.4/soal1.js
+++ soalPart9.4/soal1.js
@@ -18,39 +18,46 @@ misal faktornya angka 1 hanyalah
 1 * 1 = 2 digit (11)
 
 Return 2
-
-Pisahkan dulu faktornya
+*/
+/*
+  ========================ALGORITMA========================
+  - Jika angka hanya 1 digit otomatis hanya ada 2 digit pembagi, maka kita buat terlebih dahulu if condition nya
+  - Pisahkan atau tampilkan dahulu faktor perkalian dari parameter yang menerima argumen
+  - Gabungkan pisahan tadi agar menjadi faktor perkalian dari parameter yang menerima argumen angka
+  - Cari index yang paling sedikit diantara pisahan yang telah digabungkan tadi
 */
 
 function digitPerkalianMinimum(angka) {
-  for (i = 1; i <= 10; i++) {
+  for (i = 1; i <= 10; i++) { // Langkah pertama
     if (i === angka) {
       return 2
     }
   }
 
-  let gabung = [];
+  let gabung = []; // Langkah kedua
   let jumlah = [];
-  for (let i = 1; i <= angka; i++) {
-    if (angka % i === 0) {
-      jumlah.push(i);
+  for (let i = 1; i <= angka; i++) { 
+    if (angka % i === 0) { 
+      jumlah.push(i); // [1, 2, 3, 4, 6, 8, 12, 24]
     }
   }
-  for (let i = 0; i < jumlah.length; i++) {
-    for (let j = i + 1; j < jumlah.length; j++) {
-      if (jumlah[i] * jumlah[j] === angka) {
-        gabung.push(`${jumlah[i]}${jumlah[j]}`);
+
+  for (let i = 0; i < jumlah.length; i++) { // Langkah ketiga
+    for (let j = i + 1; j < jumlah.length; j++) { // j = i(0) + 1; j < jumlah.length (8); j++
+      if (jumlah[i] * jumlah[j] === angka) { // Misal looping sudah memasuki looping kedua. jumlah[i](2) * jumlah[j](12) === angka === 212
+        gabung.push(`${jumlah[i]}${jumlah[j]}`); // [ '124', '212', '38', '46' ]
       }
     }
   }
 
-  let panjangTerkecil = gabung[0].length;
+  // Langkah keempat
+  let panjangTerkecil = gabung[0].length; // panjangTerkecil = gabung[0].length = 124.length = 3
 
-  for (let i = 1; i < gabung.length; i++) {
-    const panjangSaatIni = gabung[i].length;
+  for (let i = 1; i < gabung.length; i++) { // i = 1; 1 < 4; i++
+    const panjangSaatIni = gabung[i].length; // panjangSaatIni = gabung[0].length = 3; gabung[1].length = 3; gabung[2].length = 2; gabung[3].length = 2
 
-    if (panjangSaatIni < panjangTerkecil) {
-      panjangTerkecil = panjangSaatIni;
+    if (panjangSaatIni < panjangTerkecil) { // 3 < 3 = false; 3 < 3 = false; 2 < 3 = true; 2 < 3 = true 
+      panjangTerkecil = panjangSaatIni; // 3 = 2
     }
   }
   return panjangTerkecil
