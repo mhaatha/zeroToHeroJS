d27261c4aa073e0702ed1b0173c756f5d2bf3f27
mhaatha <mhafidza528@gmail.com>
2023-10-20 17:20:37 +0800

    soal 4

---
+++ quizRecursion/soal4.js
@@ -0,0 +1,24 @@
+/*
+Diberikan sebuah function totalDigitRekursif(angka) yang menerima satu parameter berupa angka. Function akan me-return nilai total dari digit tersebut dengan menambahkan satu per satu angka dari digit paling depan ke paling belakang. Wajib menggunakan rekursif untuk berlatih rekursif! Disarankan untuk tidak menggunakan looping for untuk soal ini.
+
+Contoh:
+
+512, berarti outputnya adalah 8, karena 5 + 1 + 2 = 8. 1542, berarti outputnya adalah 12, karena 1 + 5 + 4 + 2 = 12.
+*/
+
+function totalDigitRekursif(angka) {
+  if (angka < 10) { // Base case
+    return angka; 
+  } else {
+    const digitTerakhir = angka % 10; // 512 % 10 = 2
+    const sisaDigit = Math.floor(angka / 10); // 512 / 10 = 51
+    return digitTerakhir + totalDigitRekursif(sisaDigit) // 2 + 51 ???????????????????????????????/
+  }
+}
+
+// TEST CASES
+console.log(totalDigitRekursif(512)); // 8
+console.log(totalDigitRekursif(1542)); // 12
+console.log(totalDigitRekursif(5)); // 5
+console.log(totalDigitRekursif(21)); // 3
+console.log(totalDigitRekursif(11111)); // 5
