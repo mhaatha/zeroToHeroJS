4993b9263438a1054e862f1f2905e84a592c1496
mhaatha <mhafidza528@gmail.com>
2023-11-13 17:33:18 +0800

    soal 2

---
+++ prepPhase1/fuck*ngRecursive/soal2.js
@@ -0,0 +1,44 @@
+/**
+ * ////////////////
+ * changeXRecursive
+ * ////////////////
+ * Function ini harus dikerjakan dengan menggunakan rekursif.
+ * Function ini menerima dua parameter, yaitu:
+ *  1. data yang merupakan string dari number
+ *  2. jenis yang merupakan string berisi antara ganjil dan genap
+ *
+ * [EXAMPLE]
+ * Input: data = 012345678922468 dan jenis = ganjil
+ * Process: Karena jenisnya ganjil maka setiap bilangan genap diubah menjadi x
+ * Output: x1x3x5x7x9xxxxx
+ *
+ * [RULES]
+ * 1. Dilarang mengubah tipe parameter function
+ * 2. Dilarang membuat function diluar function yang disediakan
+ * 5. Wajib menggunakan rekursif
+ */
+
+function changeXRecursive(data, jenis) {
+  let result = "";
+  if (data.length > 0) {
+    if (jenis === "ganjil") {
+      if (data[0] % 2 === 1) {
+        result += data[0];
+      } else {
+        result += "x";
+      }
+      result += changeXRecursive(data.slice(1), jenis);
+    } else if (jenis === "genap") {
+      if (data[0] % 2 === 0) {
+        result += data[0];
+      } else {  
+        result += "x";
+      }
+      result += changeXRecursive(data.slice(1), jenis);
+    }
+  }
+  return result;
+}
+
+console.log(changeXRecursive("012345678922468", "ganjil")); //x1x3x5x7x9xxxxx
+console.log(changeXRecursive("0123456789", "genap")); //0x2x4x6x8x
