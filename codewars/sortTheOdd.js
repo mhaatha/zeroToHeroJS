cb7ebbdefb0d6057d621417c3a92f41dda9123f5
mhaatha <mhafidza528@gmail.com>
2024-03-12 09:06:46 +0800

    Soal Codewars 8/30

---
+++ codewars/sortTheOdd.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/578aa45ee9fd15ff4600090d/train/javascript
+
+function sortArray(array) {
+  const odd = array.filter((x) => x % 2).sort((a, b) => a - b);
+  return array.map((x) => (x % 2 ? odd.shift() : x));
+}
+
+console.log(sortArray([7, 1])); // [1, 7]
+console.log(sortArray([5, 8, 6, 3, 4])); // [3, 8, 6, 5, 4]
+// console.log(sortArray([9, 8, 7, 6, 5, 4, 3, 2, 1, 0])); // [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
