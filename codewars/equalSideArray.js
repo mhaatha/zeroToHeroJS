6e60173fe306298be243f7d4971258b53552261d
mhaatha <mhafidza528@gmail.com>
2024-03-22 11:44:16 +0800

    Soal Codewars 14/30 modified

--- codewars/equalSideArray.js
+++ codewars/equalSideArray.js
@@ -26,4 +26,4 @@ const findEvenIndex = (arr) => {
 
 console.log(findEvenIndex([1, 2, 3, 4, 3, 2, 1])); // 3
 console.log(findEvenIndex([1, 100, 50, -51, 1, 1])); // 1
-console.log(findEvenIndex([20,10,-80,10,10,15,35])); // 1
+console.log(findEvenIndex([20,10,-80,10,10,15,35])); // 0
