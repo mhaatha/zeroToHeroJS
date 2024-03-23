568135a760386faf5f18cbe5ccdeff82d06fd0de
mhaatha <mhafidza528@gmail.com>
2024-03-23 22:10:34 +0800

    Soal Codewars 16/30

---
+++ codewars/findUniqueNumber.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/585d7d5adb20cf33cb000235/train/javascript
+
+const findUnique = (arr) => {
+	const objContainer = {};
+	for (let i = 0; i < arr.length; i++) {
+		if (objContainer[arr[i]] === undefined) {
+			objContainer[arr[i]] = 1;
+		} else {
+			objContainer[arr[i]]++;
+		}
+	}
+	const toArray = Object.entries(objContainer);
+	const filtered = toArray.filter(([key, value]) => value === 1);
+	return Number(filtered[0][0]);
+};
+
+console.log(findUnique([1, 1, 1, 2, 1, 1]));
+console.log(findUnique([0, 0, 0.55, 0, 0]));
